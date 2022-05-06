c7b9204eb98bacd058a561d44b12a5c14b9c1975
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-05-06 21:05:03 +0200

    initial commit

---
+++ Practice_ATMega32/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 5/6/2022 8:50:21 PM
+ *  Author: Mohamed
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
