17f4a59670e3b0f46e7145dbdfb0bfdc58ad3823
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-05-07 13:52:43 +0200

    Adding files for KeyPad drivers

---
+++ Practice_ATMega32/HAL/KeyPad.c
@@ -0,0 +1,6 @@
+/*
+ * KeyPad.c
+ *
+ * Created: 07/05/2022 13:49:08
+ *  Author: Mohamed
+ */ 
---
+++ Practice_ATMega32/HAL/KeyPad.h
@@ -0,0 +1,16 @@
+/*
+ * KeyPad.h
+ *
+ * Created: 07/05/2022 13:49:18
+ *  Author: Mohamed
+ */ 
+
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+
+u8 KEYPAD_GetEntry(void);
+
+
+#endif /* KEYPAD_H_ */
