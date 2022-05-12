fc2d190b5f70923e669cad5f02f2997b629882a4
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-05-13 00:13:03 +0200

    started calculator project

---
+++ Practice_ATMega32/APP/Calculator.c
@@ -0,0 +1,6 @@
+/*
+ * Calculator.c
+ *
+ * Created: 13/05/2022 00:08:07
+ *  Author: Mohamed
+ */ 
---
+++ Practice_ATMega32/APP/Calculator.h
@@ -0,0 +1,16 @@
+/*
+ * Calculator.h
+ *
+ * Created: 13/05/2022 00:10:30
+ *  Author: Mohamed
+ */ 
+
+
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+
+
+
+
+#endif /* CALCULATOR_H_ */
