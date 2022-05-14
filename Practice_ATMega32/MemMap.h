860dab4d21c939a538f574ce8986038ed2efe14d
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-05-15 01:24:02 +0200

    Updated the memory map for the ADC

--- Practice_ATMega32/MemMap.h
+++ Practice_ATMega32/MemMap.h
@@ -9,20 +9,36 @@
 #ifndef MEMMAP_H_
 #define MEMMAP_H_
 
-#define PORTA   (*(volatile unsigned char*)0x3B)
-#define DDRA   (*(volatile unsigned char*)0x3A)
-#define PINA   (*(volatile unsigned char*)0x39)
+/**********************************DIO Registers*************************************/
 
-#define PORTB   (*(volatile unsigned char*)0x38)
-#define DDRB   (*(volatile unsigned char*)0x37)
-#define PINB   (*(volatile unsigned char*)0x36)
+#define		PORTA		(*(volatile unsigned char*)0x3B)
+#define		DDRA		(*(volatile unsigned char*)0x3A)
+#define		PINA		(*(volatile unsigned char*)0x39)
 
-#define PORTC   (*(volatile unsigned char*)0x35)
-#define DDRC   (*(volatile unsigned char*)0x34)
-#define PINC   (*(volatile unsigned char*)0x33)
+#define		PORTB		(*(volatile unsigned char*)0x38)
+#define		DDRB		(*(volatile unsigned char*)0x37)
+#define		PINB		(*(volatile unsigned char*)0x36)
 
-#define PORTD   (*(volatile unsigned char*)0x32)
-#define DDRD   (*(volatile unsigned char*)0x31)
-#define PIND   (*(volatile unsigned char*)0x30)
+#define		PORTC		(*(volatile unsigned char*)0x35)
+#define		DDRC		(*(volatile unsigned char*)0x34)
+#define		PINC		(*(volatile unsigned char*)0x33)
+
+#define		PORTD		(*(volatile unsigned char*)0x32)
+#define		DDRD		(*(volatile unsigned char*)0x31)
+#define		PIND		(*(volatile unsigned char*)0x30)
+
+
+/**********************************ADC Registers*************************************/
+
+#define		ADMUX		(*(volatile unsigned char*)0x27)
+#define		REFS1		7
+#define		REFS0		6
+#define		ADLER		5
+#define		ADCRSA		(*(volatile unsigned char*)0x26)
+#define		ADH			(*(volatile unsigned char*)0x25)
+#define		ADL			(*(volatile unsigned char*)0x25)
+
+
+
 
 #endif /* MEMMAP_H_ */
