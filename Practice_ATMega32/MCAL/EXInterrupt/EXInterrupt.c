f01eaefff98e6545ce70fa37cfd63960b29ce466
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-06-12 16:31:34 +0200

    added function prototypes for interrupts

---
+++ Practice_ATMega32/MCAL/EXInterrupt/EXInterrupt.c
@@ -0,0 +1,6 @@
+/*
+ * EXInterrupt.c
+ *
+ * Created: 12/06/2022 16:22:16
+ *  Author: Mohamed
+ */ 
---
+++ Practice_ATMega32/MCAL/EXInterrupt/Exinterrupt.h
@@ -0,0 +1,16 @@
+/*
+ * Exinterrupt.h
+ *
+ * Created: 12/06/2022 16:22:30
+ *  Author: Mohamed
+ */ 
+
+
+#ifndef EXINTERRUPT_H_
+#define EXINTERRUPT_H_
+
+
+
+
+
+#endif /* EXINTERRUPT_H_ */
