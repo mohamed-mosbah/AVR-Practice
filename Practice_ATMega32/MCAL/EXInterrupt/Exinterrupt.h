9ba99a8d412286564c4be97d486a3f7fa98e316b
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-06-18 23:34:16 +0200

    added implementation for the external interrupt driver

--- Practice_ATMega32/MCAL/EXInterrupt/Exinterrupt.h
+++ Practice_ATMega32/MCAL/EXInterrupt/Exinterrupt.h
@@ -9,6 +9,26 @@
 #ifndef EXINTERRUPT_H_
 #define EXINTERRUPT_H_
 
+#include "StdTypes.h"
+#include "MemMap.h"
+#include "Utils.h"
+
+typedef enum{
+	LOW_LEVEL=0,
+	ANY_LOGIC_CHANGE,
+	FALLING_EDGE,
+	RISING_EDGE,
+}TriggerEdge_type;
+
+typedef enum{
+	EX_INT0=0,
+	EX_INT1,
+	EX_INT2
+}ExInterruptSource_type;
+
+void EXI_Enable(ExInterruptSource_type Interrupt);
+void EXI_Disable(ExInterruptSource_type Interrupt);
+void EXI_TriggerEdge(ExInterruptSource_type Interrupt,TriggerEdge_type Edge);
 
 
 
