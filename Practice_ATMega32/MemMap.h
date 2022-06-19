56f003c555701140dc96b827801532f87e9ee578
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-06-19 16:37:19 +0200

    Extended Memory Map with timer0 definitions

--- Practice_ATMega32/MemMap.h
+++ Practice_ATMega32/MemMap.h
@@ -85,7 +85,39 @@ void vector (void)
 #define INT1_vect			__vector_2
 /* External Interrupt Request 2 */
 #define INT2_vect			__vector_3
+/* Timer/Counter0 Overflow */
+#define TIMER0_OVF_vect			__vector_11
 
+
+/*************************************************************************************/
+/* Timer 0 */
+#define TCNT0   (*(volatile unsigned char*)0x52)
+#define TCCR0   (*(volatile unsigned char*)0x53)
+/* TCCR0 */
+#define FOC0    7
+#define WGM00   6
+#define COM01   5
+#define COM00   4
+#define WGM01   3
+#define CS02    2
+#define CS01    1
+#define CS00    0
+
+#define TWCR    (*(volatile unsigned char*)0x56)
+#define SPMCR   (*(volatile unsigned char*)0x57)
+#define TIFR    (*(volatile unsigned char*)0x58)
+#define TIMSK   (*(volatile unsigned char*)0x59)
+/* TIMSK */
+#define OCIE2   7
+#define TOIE2   6
+#define TICIE1  5
+#define OCIE1A  4
+#define OCIE1B  3
+#define TOIE1   2
+#define OCIE0   1
+#define TOIE0   0
+
+#define OCR0    (*(volatile unsigned char*)0x5C)
 
 
 
