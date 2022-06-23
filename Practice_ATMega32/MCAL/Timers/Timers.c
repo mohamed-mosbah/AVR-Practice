ec30074e20072903c391d0d7dd5984c5fb4ed372
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-06-23 18:25:16 +0200

    added files for Timers driver

---
+++ Practice_ATMega32/MCAL/Timers/Timers.c
@@ -0,0 +1,6 @@
+/*
+ * Timers.c
+ *
+ * Created: 23/06/2022 18:23:41
+ *  Author: Mohamed
+ */ 
---
+++ Practice_ATMega32/MCAL/Timers/Timers.h
@@ -0,0 +1,16 @@
+/*
+ * Timers.h
+ *
+ * Created: 23/06/2022 18:23:55
+ *  Author: Mohamed
+ */ 
+
+
+#ifndef TIMERS_H_
+#define TIMERS_H_
+
+
+
+
+
+#endif /* TIMERS_H_ */
