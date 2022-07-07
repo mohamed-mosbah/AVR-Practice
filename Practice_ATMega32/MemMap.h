c02b4f23a91289cda24868b6ba61fb31d9a92647
mohamed-mosbah <97165172+mohamed-mosbah@users.noreply.github.com>
2022-07-08 00:03:14 +0200

    Updated the memory map for SPI

--- Practice_ATMega32/MemMap.h
+++ Practice_ATMega32/MemMap.h
@@ -30,6 +30,30 @@
 
 
 /*************************************************************************************/
+/* SPI */
+/* SPI Control Register */
+#define SPCR       (*(volatile unsigned char*)0x2D)
+/* SPI Status Register */
+#define SPSR       (*(volatile unsigned char*)0x2E)
+/* SPI I/O Data Register */
+#define SPDR       (*(volatile unsigned char*)0x2F)
+
+/* SPI Status Register - SPSR */
+#define    SPIF         7
+#define    WCOL         6
+#define    SPI2X        0
+
+/* SPI Control Register - SPCR */
+#define    SPIE         7
+#define    SPE          6
+#define    DORD         5
+#define    MSTR         4
+#define    CPOL         3
+#define    CPHA         2
+#define    SPR1         1
+#define    SPR0         0
+
+/*************************************************************************************/
 /* ADC Registers */
 
 #define		ADMUX		(*(volatile unsigned char*)0x27)
